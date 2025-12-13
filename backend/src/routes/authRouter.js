23c60292827af97c15704401eccf9482d645aa2d
sainiu692 <sainiu692@gmail.com>
2025-12-13 18:02:18 +0530

    feat: returns an email Id in response

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
+
 const authRouter = express.Router();
 
 authRouter.post("/register", (req, res) => {
-  return res.status(201).send();
+  const { emailId } = req.body;
+  res.status(201).json({
+    emailId,
+  });
 });
 
 module.exports = authRouter;
