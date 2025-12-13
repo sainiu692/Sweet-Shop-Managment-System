5ca2fd427aeaa2f63b9c6e7a569c63c8f44e333f
sainiu692 <sainiu692@gmail.com>
2025-12-14 00:23:25 +0530

    made logout api

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -74,4 +74,24 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout", async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      path: "/", 
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Logout failed",
+    });
+  }
+});
+
+
 module.exports = authRouter;
